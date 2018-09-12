a0daa9c516ae2301feab32e0583e112eb6903ad9
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2018-09-11 21:18:35 -0500

    Added HWmap and DriverOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/standardDriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/standardDriveOpMode.java
@@ -1,4 +1,96 @@
 package org.firstinspires.ftc.teamcode;
 
-public class standardDriveOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+@TeleOp(name="standardDrive", group="Linear Opmode")
+
+public class standardDriveOpMode extends LinearOpMode{
+
+    standardHWMap hardware = new standardHWMap();
+
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+        float left;
+        float right;
+
+        double multiplier = .7;
+
+        hardware.init(hardwareMap);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            left = gamepad1.left_stick_y;
+            right = -(gamepad1.right_stick_y);
+
+
+            //drive
+            hardware.leftDrive.setPower(multiplier*left);
+            hardware.rightDrive.setPower(multiplier*right);
+
+            //EMBRACE THE JANK
+
+            //winch
+            if (gamepad2.right_bumper) {
+                hardware.armWinch.setPower(1);
+            } else if (gamepad2.left_bumper) {
+                hardware.armWinch.setPower(-1);
+            } else {
+                hardware.armWinch.setPower(0);
+            }
+
+            //claws
+            if (gamepad2.right_trigger > .05){
+                hardware.clawServo1.setPosition(90);
+                hardware.clawServo2.setPosition(90);
+                hardware.clawServo3.setPosition(0);
+                hardware.clawServo4.setPosition(0);
+
+            }
+            if (gamepad2.left_trigger > .05){ //inverse of above
+                hardware.clawServo1.setPosition(0);
+                hardware.clawServo2.setPosition(0);
+                hardware.clawServo3.setPosition(90);
+                hardware.clawServo4.setPosition(90);
+
+            }
+
+            //fast and slow mode for drive
+            if (gamepad1.a){
+                if (multiplier == 1.0){
+                    multiplier = .7;
+                }else if (multiplier == .7){
+                    multiplier = .5;
+                }else if (multiplier == .5){
+                    multiplier = .3;
+                }
+            }
+            if (gamepad1.b){ //inverse of above
+                if (multiplier == .7){
+                    multiplier = 1.0;
+                }else if (multiplier == .5){
+                    multiplier = .7;
+                }else if (multiplier == .3){
+                    multiplier = .5;
+                }
+            }
+            //WILL DESTROY SERVOS IF ABUSED
+            //if (gamepad2.right_trigger >= 0.1) {
+            //    hardware.clawServo1.setPosition(clawGrip1++);
+            //    hardware.clawServo2.setPosition(clawGrip2++);
+            //    hardware.clawServo3.setPosition(-(clawGrip3++));
+            //    hardware.clawServo4.setPosition(-(clawGrip4++));
+            //} else if (gamepad2.left_trigger >= 0.1) {
+            //    hardware.clawServo1.setPosition(-(clawGrip1++));
+            //    hardware.clawServo2.setPosition(-(clawGrip2++));
+            //    hardware.clawServo3.setPosition(clawGrip3++);
+            //    hardware.clawServo4.setPosition(clawGrip4++);
+            //}
+
+        }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/standardHWMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/standardHWMap.java
@@ -1,4 +1,68 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
 public class standardHWMap {
+    //something with Motors
+    public DcMotor leftDrive    = null;
+    public DcMotor rightDrive   = null;
+    public DcMotor armWinch     = null;
+
+    //we got some dope servos
+    public Servo clawServo1     = null;
+    public Servo clawServo2     = null;
+    public Servo clawServo3     = null;
+    public Servo clawServo4     = null;
+    public Servo jewelStick     = null;
+    //youre mom gay
+
+    public ColorSensor color_sensor = null;
+
+    HardwareMap defaultHwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+    //Idk what this is but dont remove
+    //ok
+    public standardHWMap() {
+
+    }
+    //HARDWARE INTERFACES AND MAPPING FOR REALS
+    //my name is jeff
+    public void init (HardwareMap updateHwMap) {
+        defaultHwMap = updateHwMap;
+
+        //Find and Init Motors
+        leftDrive       = defaultHwMap.get(DcMotor.class, "leftMotor");
+        rightDrive      = defaultHwMap.get(DcMotor.class, "rightMotor");
+        armWinch        = defaultHwMap.get(DcMotor.class, "winchMotor");
+
+        //Find and Init servos
+        clawServo1      = defaultHwMap.get(Servo.class, "clawTopRight");
+        clawServo2      = defaultHwMap.get(Servo.class, "clawBottomRight");
+        clawServo3      = defaultHwMap.get(Servo.class, "clawTopLeft");
+        clawServo4      = defaultHwMap.get(Servo.class, "clawBottomLeft");
+        jewelStick      = defaultHwMap.get(Servo.class, "jewelStick");
+
+        //set power to motors on power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        armWinch.setPower(0);
+
+        //Position Servos
+        clawServo1.setPosition(180);
+        clawServo2.setPosition(180);
+        clawServo3.setPosition(0);
+        clawServo4.setPosition(0);
+
+        jewelStick.setPosition(180);
+
+        //here us yoiu r v
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armWinch.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
 }
